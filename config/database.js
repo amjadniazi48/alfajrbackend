33b879f744cb1fde29395eb8b4bc40a36a443492
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2024-02-03 02:22:55 +0500

    change the database.js file after migration

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "containers-us-west-77.railway.app"),
-      port: env.int("DATABASE_PORT", 7644),
+      host: env("DATABASE_HOST", "viaduct.proxy.rlwy.net"),
+      port: env.int("DATABASE_PORT", 39346),
       database: env("DATABASE_NAME", "railway"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "ATtg8wBonTC38Q71Za5d"),
+      password: env("DATABASE_PASSWORD", "CaGDef6DfgddGFc6feAeb32BD4g4CcCD"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
@@ -16,7 +16,7 @@ module.exports = ({ env }) => ({
 //     client: "postgres",
 //     connection: {
 //       host: env("DATABASE_HOST", "127.0.0.1"),
-//       port: env.int("DATABASE_PORT", 5432),
+//       port: env.int("DATABASE_PORT", 39346),
 //       database: env("DATABASE_NAME", "alfajr"),
 //       user: env("DATABASE_USERNAME", "postgres"),
 //       password: env("DATABASE_PASSWORD", "postgres"),
@@ -24,3 +24,10 @@ module.exports = ({ env }) => ({
 //     },
 //   },
 // });
+//postgresql://postgres:CaGDef6DfgddGFc6feAeb32BD4g4CcCD@viaduct.proxy.rlwy.net:39346/railway
+//  host: env("DATABASE_HOST", "containers-us-west-77.railway.app"),
+// port: env.int("DATABASE_PORT", 7644),
+// database: env("DATABASE_NAME", "railway"),
+// user: env("DATABASE_USERNAME", "postgres"),
+// password: env("DATABASE_PASSWORD", "ATtg8wBonTC38Q71Za5d"),
+// ssl: env.bool("DATABASE_SSL", false),
